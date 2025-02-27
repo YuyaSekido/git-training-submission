fdb2f606dabdd03e5afef8de4871e3bfd5193eb4
sekidoyuya <sekidoyuya@localhost>
2025-02-27 10:16:23 +0900

    2/26分コミット

--- samuraitravel/src/main/java/com/example/samuraitravel/entity/Reservation.java
+++ samuraitravel/src/main/java/com/example/samuraitravel/entity/Reservation.java
@@ -1,8 +1,12 @@
 package com.example.samuraitravel.entity;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -14,10 +18,33 @@ import lombok.Data;
 @Data
 public class Reservation {
 	@Id
-	@GeneratedValue(strategy = GeberationType.IDENTITY)
-	@Column(name = "id")private Integer id;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
 	
 	@ManyToOne
 	@JoinColumn(name = "house_id")
 	private House house;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@Column(name = "checkin_date")
+	private LocalDate checinDate;
+	
+	@Column(name = "checkout_date")
+	private LocalDate checkoutDate;
+	
+	@Column(name = "number_of_people")
+	private Integer numberOfPeople;
+	
+	@Column(name = "amount")
+	private Integer amount;
+	
+	@Column(name = "created_at", insertable = false, updatable = false)
+	private Timestamp createdAt;
+	
+	@Column(name = "updated_at", insertable = false, updatable = false)
+	private Timestamp updatedAt;
 }
