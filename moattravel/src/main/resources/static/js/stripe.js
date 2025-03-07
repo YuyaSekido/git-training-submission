95b9a42528a304c49e2b24e4d2932e7b388e74d2
sekidoyuya <sekidoyuya@localhost>
2025-03-07 13:11:47 +0900

    3/7 プッシュ

---
+++ moattravel/src/main/resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+const stripe = Stripe('pk_test_51QwwMSDHMOkIgiDFnaQegzDCHj0zsE7dVrQdBjD1CTEiDm63bmXGEjdZfjjaYuFreQXZWzxBD0IqGpCritAxTwUY00AyMEPOA');
+const paymentButton = document.querySelictor('#paymentButton');
+
+peymentButton.addEventListener('click', () => {
+	stripe.redirectToCheckout({
+		sessionId: sesiionId
+	})
+});
