cfdb3d82788dba07ff4c8b250aa45dd5d2ed2214
sekidoyuya <sekidoyuya@localhost>
2025-03-09 13:47:16 +0900

    3/9 14時プッシュ

---
+++ seckeytravel/src/main/java/com/example/moattravel/SeckeytravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.moattravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeckeytravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckeytravelApplication.class, args);
+	}
+
+}
